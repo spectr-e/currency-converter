90b317434c95caf7d2f24cc01b525c413ca2eecf
josiah <devs.josia@gmail.com>
2022-12-20 16:19:11 +0300

    Working on the input element

--- src/app.js
+++ src/app.js
@@ -23,28 +23,28 @@ function currencySelect(rates) {
       const option = document.createElement("option");
       option.value = curr;
       option.textContent = curr;
-      currency[i].appendChild(option)
+      currency[i].appendChild(option);
     }
-    currency[i].addEventListener('change', (e)=>{
-          displayRates(rates);
-          changeFlag(e.target)
-    })
+    currency[i].addEventListener("change", (e) => {
+      displayRates(rates);
+      changeFlag(e.target);
+    });
   }
 }
 
 function displayRates(rates) {
   const from = inputCur.value;
   const to = outputCur.value;
-  const have = ((1/rates[from]) / (1/rates[to])).toFixed(4)
-  const want = ((1/rates[to]) / (1/rates[from])).toFixed(4)
+  const have = (1 / rates[from] / (1 / rates[to])).toFixed(4);
+  const want = (1 / rates[to] / (1 / rates[from])).toFixed(4);
   rateFrom.textContent = `1 ${from} = ${have} ${to}`;
   rateTo.textContent = `1 ${to} = ${want} ${from}`;
 }
-function changeFlag(target){
-    for (code in country){
-        if (code === target.value){
-            const flag = target.parentElement.querySelector('img')
-            flag.src = `https://flagsapi.com/${country[code]}/flat/64.png`
-        }
+function changeFlag(target) {
+  for (code in country) {
+    if (code === target.value) {
+      const flag = target.parentElement.querySelector("img");
+      flag.src = `https://flagsapi.com/${country[code]}/flat/64.png`;
     }
+  }
 }
